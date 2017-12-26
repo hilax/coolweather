f036dc14da1bd205e0265d0e18354c9495f2bb76
SxigL <1534358301@qq.com>
2017-12-26 17:18:19 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/neusoft/coolweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.neusoft.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Hilax on 2017/12/26.
+ */
+
+public class HttpUtil {
+    public static  void sengOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
